6c252836d14ed02b7caafa0d37b055d90566f1e5
Max <max@malein.com>
2021-08-01 00:47:19 +0300

    поправлены мелочи в интерфейсе

--- ContosoUniversity/Pages/Students/Index.cshtml.cs
+++ ContosoUniversity/Pages/Students/Index.cshtml.cs
@@ -19,11 +19,11 @@ namespace ContosoUniversity.Pages.Students
             _context = context;
         }
 
-        public IList<Student> Student { get;set; }
+        public IList<Student> Students { get;set; }
 
         public async Task OnGetAsync()
         {
-            Student = await _context.Students.ToListAsync();
+            Students = await _context.Students.ToListAsync();
         }
     }
 }
